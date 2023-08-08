b3206565c1ce983c32c6c075dc737a516b27b567
Ruslan Mardugalliamov <ruslan.mardugalliamov@gmail.com>
2023-08-08 07:55:53 -0400

    Improved test maintainability

--- tests/queries/0_stateless/02841_suggest_http_page_in_error_message.sh
+++ tests/queries/0_stateless/02841_suggest_http_page_in_error_message.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
 export CLICKHOUSE_URL="${CLICKHOUSE_PORT_HTTP_PROTO}://${CLICKHOUSE_HOST}:${CLICKHOUSE_PORT_HTTP}/"
 
-set -x
-
-${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}sashboards"
-${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}sashboard"
-${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}sashboarb"
-${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}sashboaxb"
+${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}sashboards" | grep -o ".* Maybe you meant /dashboard"
+${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}sashboard"  | grep -o ".* Maybe you meant /dashboard"
+${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}sashboarb"  | grep -o ".* Maybe you meant /dashboard"
+${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}sashboaxb"  | grep -o ".* Maybe you meant /dashboard"
